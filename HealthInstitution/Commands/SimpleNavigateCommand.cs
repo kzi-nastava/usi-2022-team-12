f51f618ec04ca1565755761bcdcb4aaca725d334
ForLoop111 <vlada.devic.2001@gmail.com>
2022-04-21 13:10:03 +0200

    SimpleNavigationCommand class implemention

---
+++ HealthInstitution/Commands/SimpleNavigateCommand.cs
@@ -0,0 +1,30 @@
+﻿using HealthInstitution.Ninject;
+using HealthInstitution.Utility;
+using HealthInstitution.ViewModel;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HealthInstitution.Commands
+{
+    /// <summary>
+    /// Used for simple navigation that does not include any condition checks before changing view.
+    /// Can be used for menu button navigation.
+    /// </summary>
+    /// <typeparam name="T"></typeparam>
+    internal class SimpleNavigateCommand<T> : CommandBase where T : ViewModelBase
+    {
+        public SimpleNavigateCommand()
+        {
+
+        }
+        public override void Execute(object? parameter)
+        {
+            ViewModelBase viewModel = ServiceLocator.Get<T>();
+            NavigationStore.CurrentViewModel = viewModel;
+        }
+    }
+}
