ce3225df5962f51e22abc8abd3bd9228be08c92a
ForLoop111 <vlada.devic.2001@gmail.com>
2022-04-21 10:57:20 +0200

    Implementation of DoctorHomeViewModel

---
+++ HealthInstitution/ViewModel/DoctorHomeViewModel.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+
+namespace HealthInstitution.ViewModel
+{
+    internal class DoctorHomeViewModel : ViewModelBase
+    {
+        public ICommand? LogoutCommand { get; }
+        public ICommand? NavigateDoctorScheduleOverviewCommand { get; }
+
+        public DoctorHomeViewModel()
+        {
+            LogoutCommand = new LogoutCommand();
+
+            //TODO Make NavigateCommand template class that should change window to corresponding one 
+            //NavigateDoctorScheduleOverviewCommand = new NavigateCommand<>()
+        }
+    }
+}
