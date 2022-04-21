9a16e1596702dae917aff908f15ad5c66c1e8a39
ForLoop111 <vlada.devic.2001@gmail.com>
2022-04-21 11:50:43 +0200

    Anamnesis model fix
    Reverting Anamnesis model to original form due to dependency problems when adding Appointment field to it.

--- HealthInstitution/Model/Appointment/Anamnesis.cs
+++ HealthInstitution/Model/Appointment/Anamnesis.cs
@@ -3,8 +3,6 @@
     public class Anamnesis : BaseObservableEntity
     {
         #region Attributes
-        private Appointment _appointment;
-        public Appointment? Appointment { get => _appointment; set => OnPropertyChanged(ref _appointment, value); }
 
         private string _description;
         public string Description { get => _description; set => OnPropertyChanged(ref _description, value); }
@@ -13,10 +11,9 @@
 
         #region Constructor
 
-        public Anamnesis(string description, Appointment appointment)
+        public Anamnesis(string description)
         {
             Description = description;
-            Appointment = appointment;
         }
 
         #endregion
