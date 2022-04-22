8e31136dd1733f14dca4858f977f72f71fd23e0c
jokicjovan <jokic.sv47.2020@uns.ac.rs>
2022-04-22 02:28:16 +0200

    View and ViewModel for appointment creation

---
+++ HealthInstitution/Commands/AppointmentCreationCommand.cs
@@ -0,0 +1,39 @@
+﻿using HealthInstitution.ViewModel;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HealthInstitution.Commands
+{
+    public class AppointmentCreationCommand : CommandBase
+    {
+        private readonly AppointmentCreationViewModel? _viewModel;
+
+        public AppointmentCreationCommand(AppointmentCreationViewModel acvm)
+        {
+            _viewModel = acvm;
+            _viewModel.PropertyChanged += OnViewModelPropertyChanged;
+        }
+
+        private void OnViewModelPropertyChanged(object sender, PropertyChangedEventArgs e)
+        {
+            if (e.PropertyName == nameof(_viewModel.Date) || e.PropertyName == nameof(_viewModel.Time) || e.PropertyName == nameof(_viewModel.SelectedDoctor))
+            {
+                OnCanExecuteChange();
+            }
+        }
+
+        public override bool CanExecute(object? parameter)
+        {
+            return !(_viewModel.Date < DateTime.Now) && !string.IsNullOrEmpty(_viewModel.Time) && base.CanExecute(parameter) && !(_viewModel.SelectedDoctor == null); 
+        }
+
+        public override void Execute(object? parameter)
+        {
+            
+        }
+    }
+}
--- HealthInstitution/ViewModel/AppointmentCreationViewModel.cs
+++ HealthInstitution/ViewModel/AppointmentCreationViewModel.cs
@@ -1,13 +1,85 @@
-﻿using System;
+﻿using HealthInstitution.Commands;
+using HealthInstitution.Model;
+using HealthInstitution.Services.Intefaces;
+using System;
 using System.Collections.Generic;
+using System.ComponentModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Input;
 
 namespace HealthInstitution.ViewModel
 {
     public class AppointmentCreationViewModel : ViewModelBase
     {
+        public readonly IDoctorService<Doctor> _doctorService;
+        public readonly IAppointmentService<Appointment> _appointmentService;
 
+        private DateTime? _date;
+        public DateTime? Date
+        {
+            get => _date;
+            set
+            {
+                _date = value;
+                OnPropertyChanged(nameof(Date));
+            }
+        }
+
+        private string _time;
+        public string? Time
+        {
+            get => _time;
+            set
+            {
+                _time = value;
+                OnPropertyChanged(nameof(Time));
+            }
+        }
+
+        //bindovati
+        private ComboBox _doctorComboBox;
+        public ComboBox DoctorComboBox
+        {
+            get => _doctorComboBox;
+            set
+            {
+                _doctorComboBox = value;
+                OnPropertyChanged(nameof(DoctorComboBox));
+            }
+        }
+
+        private Doctor _selectedDoctor;
+        public Doctor SelectedDoctor
+        {
+            get => _selectedDoctor;
+            set
+            {
+                _selectedDoctor = value;
+                OnPropertyChanged(nameof(SelectedDoctor));
+            }
+        }
+
+        private List<Doctor> _doctors;
+        public List<Doctor> Doctors 
+        { 
+            get => _doctors;
+            set {
+                _doctors = value;
+                OnPropertyChanged(nameof(Doctors));
+            }
+        }
+        public ICommand? AppointmentCreationCommand { get; }
+
+        public AppointmentCreationViewModel(IDoctorService<Doctor> doctorService, IAppointmentService<Appointment> appointmentService)
+        {
+            _doctorService = doctorService;
+            _appointmentService = appointmentService;
+            Doctors = doctorService.ReadAll().ToList<Doctor>();
+            AppointmentCreationCommand = new AppointmentCreationCommand(this);
+        }
     }
 }
