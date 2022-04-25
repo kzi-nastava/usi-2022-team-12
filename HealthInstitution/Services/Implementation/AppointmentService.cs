fd8e981c37121e1635ca39cab362fd13c5bea21c
ForLoop111 <vlada.devic.2001@gmail.com>
2022-04-25 02:13:21 +0200

    Added new query to AppointmentService

--- HealthInstitution/Services/Implementation/AppointmentService.cs
+++ HealthInstitution/Services/Implementation/AppointmentService.cs
@@ -1,6 +1,9 @@
 ﻿using HealthInstitution.Model;
 using HealthInstitution.Persistence;
 using HealthInstitution.Services.Intefaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
 
 namespace HealthInstitution.Services.Implementation
 {
@@ -9,6 +12,11 @@ namespace HealthInstitution.Services.Implementation
         public AppointmentService(DatabaseContext context) : base(context)
         {
 
+        }
+
+        public IEnumerable<Appointment> GetAppointmentsForDateRangeAndDoctor(DateTime start, DateTime end, Doctor doctor)
+        {
+            return _entities.Where(e => e.Doctor == doctor && e.StartDate.Date >= start.Date && e.StartDate.Date <= end.Date);
         }
     }
 }
