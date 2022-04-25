c9a06c2756c691e8044bfa946edeaf9030e123d8
ForLoop111 <vlada.devic.2001@gmail.com>
2022-04-25 20:08:45 +0200

    Added HealthRecordService

---
+++ HealthInstitution/Services/Implementation/MedicalRecordService.cs
@@ -0,0 +1,21 @@
+﻿using HealthInstitution.Model;
+using HealthInstitution.Persistence;
+using HealthInstitution.Services.Intefaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HealthInstitution.Services.Implementation
+{
+    public class MedicalRecordService : CrudService<MedicalRecord>, IMedicalRecordService
+    {
+        public MedicalRecordService(DatabaseContext context) : base(context) { }
+
+        public MedicalRecord GetMedicalRecordForPatient(Patient patient)
+        {
+            return _entities.Where(e => e.Patient == patient).FirstOrDefault();
+        }
+    }
+}
