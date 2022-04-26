e0fc7eb8888a6f6eb04e014f5076b560a19b0083
ForLoop111 <vlada.devic.2001@gmail.com>
2022-04-26 02:01:28 +0200

    Create NavigateMedicalRecordCommand

---
+++ HealthInstitution/Commands/Doctor/Navigation/NavigateMedicalRecordCommand.cs
@@ -0,0 +1,39 @@
+﻿using HealthInstitution.Utility;
+using HealthInstitution.ViewModel;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HealthInstitution.Commands
+{
+    internal class NavigateMedicalRecordCommand : CommandBase
+    {
+        private DoctorScheduleViewModel _viewModel;
+        public NavigateMedicalRecordCommand(ViewModelBase viewModel)
+        {
+            _viewModel = (DoctorScheduleViewModel)viewModel;
+            _viewModel.PropertyChanged += OnViewModelPropertyChanged;
+        }
+
+        private void OnViewModelPropertyChanged(object sender, PropertyChangedEventArgs e)
+        {
+            if (e.PropertyName == nameof(_viewModel.SelectedAppointment))
+            {
+                OnCanExecuteChange();
+            }
+        }
+
+        public override bool CanExecute(object? parameter)
+        {
+            return _viewModel.SelectedAppointment is not null && base.CanExecute(parameter);
+        }
+        public override void Execute(object? parameter)
+        {
+            GlobalStore.AddObject("SelectedPatient", _viewModel.SelectedAppointment.Patient);
+            EventBus.FireEvent("MedicalHistory");
+        }
+    }
+}
