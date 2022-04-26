05df124b2c7f804226866e56abccfdf401e5375f
ForLoop111 <vlada.devic.2001@gmail.com>
2022-04-26 21:10:55 +0200

    Created StartAppointmentCommand

---
+++ HealthInstitution/Commands/Doctor/Navigation/StartAppointmentCommand.cs
@@ -0,0 +1,41 @@
+﻿using HealthInstitution.ViewModel;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HealthInstitution.Commands
+{
+    public class StartAppointmentCommand : CommandBase
+    {
+        private DoctorScheduleViewModel _viewModel;
+        public StartAppointmentCommand(ViewModelBase viewModel)
+        {
+            _viewModel = (DoctorScheduleViewModel)viewModel;
+            _viewModel.PropertyChanged += OnViewModelPropertyChanged;
+        }
+
+        private void OnViewModelPropertyChanged(object sender, PropertyChangedEventArgs e)
+        {
+            if (e.PropertyName == nameof(_viewModel.SelectedAppointment))
+            {
+                OnCanExecuteChange();
+            }
+        }
+
+        public override bool CanExecute(object? parameter)
+        {
+            return _viewModel.SelectedAppointment is not null
+                && !_viewModel.SelectedAppointment.IsDone
+                && _viewModel.SelectedAppointment.Appointment.StartDate.Date >= DateTime.Now.Date
+                && base.CanExecute(parameter);
+        }
+
+        public override void Execute(object? parameter)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
