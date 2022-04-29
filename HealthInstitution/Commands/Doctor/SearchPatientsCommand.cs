07058b4d0e165fdc3409aa8ed514120c6daa9c18
ForLoop111 <vlada.devic.2001@gmail.com>
2022-04-29 23:38:41 +0200

    Added SearchPatientsCommand

---
+++ HealthInstitution/Commands/Doctor/SearchPatientsCommand.cs
@@ -0,0 +1,22 @@
+﻿using HealthInstitution.ViewModel;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HealthInstitution.Commands
+{
+    public class SearchPatientsCommand : CommandBase
+    {
+        private readonly IDoctorAppointmentViewModel _viewModel;
+        public SearchPatientsCommand(IDoctorAppointmentViewModel viewModel)
+        {
+            _viewModel = viewModel;
+        }
+        public override void Execute(object? parameter)
+        {
+            _viewModel.UpdateData(_viewModel.SearchText);
+        }
+    }
+}
