1ca44ea66b1975ddfff11c51956c3bad7fff1c21
ForLoop111 <vlada.devic.2001@gmail.com>
2022-05-14 11:03:27 +0200

    Created BackToExamination Command

---
+++ HealthInstitution/Commands/Doctor/BackToExaminationCommand.cs
@@ -0,0 +1,29 @@
+﻿using HealthInstitution.Model;
+using HealthInstitution.Utility;
+using HealthInstitution.ViewModel;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HealthInstitution.Commands
+{
+    public class BackToExaminationCommand : CommandBase
+    {
+        DoctorReferralCreationViewModel _viewModel;
+        public BackToExaminationCommand(DoctorReferralCreationViewModel viewModel) {
+            _viewModel = viewModel;
+        }
+        public override void Execute(object? parameter)
+        {
+            List<Referral> newReferrals = new List<Referral>();
+            foreach(Referral referral in _viewModel.Referrals)
+            {
+                newReferrals.Add(referral);
+            }
+            GlobalStore.AddObject("NewReferrals", newReferrals);
+            EventBus.FireEvent("ReturnToExamination");
+        }
+    }
+}
