f4eaf750bc756f072f57c4532bbb7853c6d4a7c2
ForLoop111 <vlada.devic.2001@gmail.com>
2022-05-14 11:06:05 +0200

    Created IssueREfferalCommand

---
+++ HealthInstitution/Commands/Doctor/IssueRefferalCommand.cs
@@ -0,0 +1,41 @@
+﻿using HealthInstitution.Model;
+using HealthInstitution.Utility;
+using HealthInstitution.ViewModel;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HealthInstitution.Commands
+{
+    public class IssueRefferalCommand : CommandBase
+    {
+        DoctorReferralCreationViewModel _viewModel;
+        public IssueRefferalCommand(DoctorReferralCreationViewModel viewModel)
+        {
+            _viewModel = viewModel;
+            _viewModel.PropertyChanged += OnViewModelPropertyChanged;
+        }
+
+        private void OnViewModelPropertyChanged(object sender, PropertyChangedEventArgs e)
+        {
+            if (e.PropertyName == nameof(_viewModel.SelectedDoctor))
+            {
+                OnCanExecuteChange();
+            }
+        }
+
+        public override bool CanExecute(object? parameter)
+        {
+            return _viewModel.SelectedDoctor is not null
+                && base.CanExecute(parameter);
+        }
+        public override void Execute(object? parameter)
+        {
+            Referral referral = new Referral { Doctor = _viewModel.SelectedDoctor, IsUsed = false, Patient = _viewModel.Patient };
+            _viewModel.addReferral(referral);
+        }
+    }
+}
