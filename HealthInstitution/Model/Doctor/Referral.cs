ecf444518aefe82a628fa90dbdda698ed4e490a0
ForLoop111 <vlada.devic.2001@gmail.com>
2022-05-14 11:11:29 +0200

    Updated Referral Model

--- HealthInstitution/Model/Doctor/Referral.cs
+++ HealthInstitution/Model/Doctor/Referral.cs
@@ -13,12 +13,12 @@ namespace HealthInstitution.Model
         private Doctor? _doctor;
         public virtual Doctor Doctor { get => _doctor; set => OnPropertyChanged(ref _doctor, value); }
 
-        private DoctorSpecialization? _specialization;
-        public DoctorSpecialization? DoctorSpecialization { get => _specialization; set => OnPropertyChanged(ref _specialization, value); }
-
         private Patient _patient;
         public virtual Patient Patient { get => _patient; set => OnPropertyChanged(ref _patient, value); }
 
+        private bool _isUsed;
+
+        public bool IsUsed { get => _isUsed; set => OnPropertyChanged(ref _isUsed, value); }
         #endregion
 
         #region Constructor
