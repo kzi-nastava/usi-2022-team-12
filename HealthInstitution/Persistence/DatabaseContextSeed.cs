8edd317540797d7b8814f76f093b6f6517743063
ForLoop111 <vlada.devic.2001@gmail.com>
2022-05-14 11:12:17 +0200

    Updated DatabaseContextSeed

--- HealthInstitution/Persistence/DatabaseContextSeed.cs
+++ HealthInstitution/Persistence/DatabaseContextSeed.cs
@@ -7,12 +7,14 @@ namespace HealthInstitution.Persistence
     {
         public static void Seed(DatabaseContext context)
         {
+
             //// Patients
             var p1 = new Patient { FirstName = "Petar", LastName = "Peric", Password = "test123", DateOfBirth = DateTime.Now.AddYears(-20), EmailAddress = "petarperic@example.com", Role = Role.Patient, IsBlocked = true };
             var p2 = new Patient { FirstName = "Marko", LastName = "Markovic", Password = "test123", DateOfBirth = DateTime.Now.AddYears(-35), EmailAddress = "markomarkovic@example.com", Role = Role.Patient, IsBlocked = false };
             var p3 = new Patient { FirstName = "Zeljko", LastName = "Nikolic", Password = "test123", DateOfBirth = DateTime.Now.AddYears(-50), EmailAddress = "example@example.com", Role = Role.Patient, IsBlocked = false };
             var p4 = new Patient { FirstName = "Milica", LastName = "Milic", Password = "test123", DateOfBirth = DateTime.Now.AddYears(-40), EmailAddress = "zeljkonikolic@example.com", Role = Role.Patient, IsBlocked = false };
             var p5 = new Patient { FirstName = "Zoran", LastName = "Gostojic", Password = "test123", DateOfBirth = DateTime.Now.AddYears(-38), EmailAddress = "zorangostojic@example.com", Role = Role.Patient, IsBlocked = false };
+            var p6 = new Patient { FirstName = "Brusli", LastName = "Iljazovic", Password = "test123", DateOfBirth = DateTime.Now.AddYears(-38), EmailAddress = "brusliiljazovic@example.com", Role = Role.Patient, IsBlocked = false };
 
             context.Patients.Add(p1);
             context.Patients.Add(p2);
@@ -27,7 +29,10 @@ namespace HealthInstitution.Persistence
             mr1.AddAllergen(alr1);
             mr1.AddIllness(il1);
 
+            var mr2 = new MedicalRecord(179, 65.6, p6);
+
             context.MedicalRecords.Add(mr1);
+            context.MedicalRecords.Add(mr2);
 
             // Secretaries
             var c1 = new Secretary { FirstName = "Nikola", LastName = "Petrovic", Password = "test123", DateOfBirth = DateTime.Now.AddYears(-55), EmailAddress = "nikolapetrovic@example.com", Role = Role.Secretary };
