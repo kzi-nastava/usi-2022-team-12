d760916f30a46f371f9f9719a5a99b0be21a2b00
ForLoop111 <vlada.devic.2001@gmail.com>
2022-05-31 20:40:00 +0200

    Created NavigateMedicineCommand

---
+++ HealthInstitution/Commands/Doctor/Navigation/NavigateMedicineCommand.cs
@@ -0,0 +1,12 @@
+﻿using HealthInstitution.Utility;
+
+namespace HealthInstitution.Commands
+{
+    public class NavigateMedicineCommand : CommandBase
+    {
+        public override void Execute(object? parameter)
+        {
+            EventBus.FireEvent("DoctorMedicineManagment");
+        }
+    }
+}
