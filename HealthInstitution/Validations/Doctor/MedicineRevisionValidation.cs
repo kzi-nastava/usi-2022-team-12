078a3af92cddea0b45dbecf574cf91b5363e3b07
ForLoop111 <vlada.devic.2001@gmail.com>
2022-05-31 20:46:42 +0200

    Created MedicineRevisionValidation

---
+++ HealthInstitution/Validations/Doctor/MedicineRevisionValidation.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Controls;
+
+namespace HealthInstitution.Validations.Doctor
+{
+    public class MedicineRevisionValidation : ValidationRule
+    {
+        public override ValidationResult Validate(object value, CultureInfo cultureInfo)
+        {
+            string? revision = value.ToString();
+            if (string.IsNullOrWhiteSpace(revision))
+            {
+                return new ValidationResult(false, "Revision comment cannot be empty");
+            }
+            return ValidationResult.ValidResult;
+        }
+    }
+}
