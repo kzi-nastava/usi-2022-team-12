da6fc9778cb6bbe844a96d5eda5c131b92b3ff14
jokicjovan <jokic.sv47.2020@uns.ac.rs>
2022-06-01 10:04:49 +0200

    Update NotificationsChecker.cs

--- HealthInstitution/Utility/NotificationsChecker.cs
+++ HealthInstitution/Utility/NotificationsChecker.cs
@@ -14,23 +14,27 @@ namespace HealthInstitution.Utility
     {
         private static Timer _notificationsTriggerTimer;
         private static Timer _notificationsUpdateTimer;
+        private static bool _databaseBusy;
         private static readonly IPrescribedMedicineNotificationService _prescribedMedicineNotificationService;
-        static NotificationsChecker() {
+        static NotificationsChecker()
+        {
             _prescribedMedicineNotificationService = ServiceLocator.Get<IPrescribedMedicineNotificationService>();
         }
 
-        public static void InitializeTimer(string loggedEntity) {
+        public static void InitializeTimer(string loggedEntity)
+        {
+            _databaseBusy = false;
             StopTimer();
 
-            AutoResetEvent autoEvent = new AutoResetEvent(true);
             if (loggedEntity == "patient")
             {
-                _notificationsUpdateTimer = new Timer(UpdatePrescribedMedicationsNotifications, autoEvent, 1000, 10000);
-                _notificationsTriggerTimer = new Timer(CheckForUpcomingNotification, autoEvent, 1000, 5000);
+                _notificationsUpdateTimer = new Timer(UpdatePrescribedMedicationsNotifications, null, 1000, Timeout.Infinite);
+                _notificationsTriggerTimer = new Timer(CheckForUpcomingNotification, null, 1000, Timeout.Infinite);
             }
         }
 
-        public static void StopTimer() {
+        public static void StopTimer()
+        {
             if (_notificationsTriggerTimer != null)
             {
                 _notificationsTriggerTimer.Dispose();
@@ -41,30 +45,41 @@ namespace HealthInstitution.Utility
             }
         }
 
-        public static void UpdatePrescribedMedicationsNotifications(Object stateInfo) {
-            AutoResetEvent autoEvent = (AutoResetEvent)stateInfo;
-            autoEvent.WaitOne();
-
-            Patient patient = GlobalStore.ReadObject<Patient>("LoggedUser");
-            _prescribedMedicineNotificationService.CreateUpcomingMedicationsNotifications(patient, patient.NotificationPreference);
-
-            autoEvent.Set();
+        public static void UpdatePrescribedMedicationsNotifications(Object stateInfo)
+        {
+            if (!_databaseBusy)
+            {
+                _databaseBusy = true;
+                Patient patient = GlobalStore.ReadObject<Patient>("LoggedUser");
+                _prescribedMedicineNotificationService.CreateUpcomingMedicationsNotifications(patient, patient.NotificationPreference);
+                _databaseBusy = false;
+                _notificationsUpdateTimer.Change(300000, Timeout.Infinite);
+            }
+            else {
+                _notificationsUpdateTimer.Change(1000, Timeout.Infinite);
+            }
         }
 
-        public static void CheckForUpcomingNotification(Object stateInfo) {
-            AutoResetEvent autoEvent = (AutoResetEvent)stateInfo;
-            autoEvent.WaitOne();
-
-            Patient patient = GlobalStore.ReadObject<Patient>("LoggedUser");
-            var nextMedicineNotification = _prescribedMedicineNotificationService.GetNextMedicineNotification(patient);
-            if (nextMedicineNotification != null && nextMedicineNotification.TriggerTime < DateTime.Now && nextMedicineNotification.Triggered == false) {
-                MessageBox.Show("You should drink " + nextMedicineNotification.PrescribedMedicine.Medicine.Name + " at " + nextMedicineNotification.TriggerTime.AddMinutes(patient.NotificationPreference) + 
-                    "\nInstructions: " + nextMedicineNotification.PrescribedMedicine.Instruction, "Reminder");
-                nextMedicineNotification.Triggered = true;
-                _prescribedMedicineNotificationService.Update(nextMedicineNotification);
+        public static void CheckForUpcomingNotification(Object stateInfo)
+        {
+            if (!_databaseBusy)
+            {
+                _databaseBusy = true;
+                Patient patient = GlobalStore.ReadObject<Patient>("LoggedUser");
+                var nextMedicineNotification = _prescribedMedicineNotificationService.GetNextMedicineNotification(patient);
+                if (nextMedicineNotification != null && nextMedicineNotification.TriggerTime < DateTime.Now)
+                {
+                    MessageBox.Show("You should drink " + nextMedicineNotification.PrescribedMedicine.Medicine.Name + " at " + nextMedicineNotification.TriggerTime.AddMinutes(patient.NotificationPreference) +
+                        "\nInstructions: " + nextMedicineNotification.PrescribedMedicine.Instruction, "Reminder");
+                    nextMedicineNotification.Triggered = true;
+                    _prescribedMedicineNotificationService.Update(nextMedicineNotification);
+                }
+                _databaseBusy = false;
+                _notificationsTriggerTimer.Change(60000, Timeout.Infinite);
             }
-
-            autoEvent.Set();
+            else {
+                _notificationsTriggerTimer.Change(1000, Timeout.Infinite);
+            }
         }
     }
 }
